e9a3ffc5fcdfd0dc110aaf52c17b496cf37d466f
하이라이트 <77767930+highright1234@users.noreply.github.com>
2022-11-08 20:20:50 +0900

    굿

--- src/main/kotlin/io/github/highright1234/humantower/TowerMaker.kt
+++ src/main/kotlin/io/github/highright1234/humantower/TowerMaker.kt
@@ -1,16 +1,22 @@
 package io.github.highright1234.humantower
 
+import com.github.shynixn.mccoroutine.bukkit.asyncDispatcher
 import com.github.shynixn.mccoroutine.bukkit.launch
 import io.github.highright1234.seniorcentertwerkmachine.util.NpcUtil
 import io.github.highright1234.shotokonoko.Shotokonoko.plugin
+import io.github.highright1234.shotokonoko.coroutine.MutableDelayData
+import io.github.highright1234.shotokonoko.coroutine.mutableDelay
 import io.github.monun.tap.fake.FakeEntity
+import io.github.monun.tap.mojangapi.MojangAPI
 import io.github.monun.tap.protocol.PacketSupport
 import io.github.monun.tap.protocol.sendPacket
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.WeakHashMap
+import java.util.concurrent.ConcurrentHashMap
 
 object TowerMaker {
 
@@ -23,18 +29,23 @@ object TowerMaker {
 
     private val passengers = WeakHashMap<Player, Player>()
 
-    suspend fun create(player: Player, isTwerking: Boolean = false) {
+    suspend fun create(player: Player, profiles: MojangAPI.SkinProfile? = null, isTwerking: Boolean = false) {
         val tail = tailOf(player)
         val location = tail.location.clone().apply {
             pitch = 0.0f
             @Suppress("MagicNumber")
             yaw += 30
         }
+        val profile = profiles ?: skinOf(player.name)
         val npc: FakeEntity<Player> = if (isTwerking) {
 //        if (isTwerking) {
-            NpcUtil.createNpc(player.name, location).getOrNull()?.fakePlayer ?: return
+            NpcUtil.createNpc(player.name, location, null, profile).getOrNull()?.fakePlayer ?: return
         } else {
-            HumanTower.fakeServer.spawnPlayer(location, "", player.playerProfile.properties)
+            HumanTower.fakeServer.spawnPlayer(
+                location,
+                "",
+                profile?.profileProperties()?.toSet() ?: emptySet()
+            )
         }
         passengers[player] = npc.bukkitEntity
         plugin.launch {
@@ -52,6 +63,44 @@ object TowerMaker {
                 }
             }
         }
+    }
+
+    private val profileImporter : MutableMap<String, Job> = ConcurrentHashMap<String, Job>()
+    private val skinCache = ConcurrentHashMap<String, MojangAPI.SkinProfile?>()
+    private val skinCacheRemover = ConcurrentHashMap<String, MutableDelayData>()
+
+
+    // 솔직히 이거 스킨 말고 다른것도 포함되는거 아는데
+    // 귀찮아서 스킨이라함
+    suspend fun skinOf(name: String): MojangAPI.SkinProfile? {
+
+        profileImporter[name]?.join()
+        skinCache[name]?.let {
+            @Suppress("MagicNumber")
+            skinCacheRemover[name]?.timeToRun = System.currentTimeMillis() + 60000L
+            return it
+        }
+
+        println("응애애")
+        var profile: MojangAPI.SkinProfile? = null
+        plugin.launch(plugin.asyncDispatcher) {
+            profile = MojangAPI.fetchProfile(name)?.uuid()?.let { MojangAPI.fetchSkinProfile(it) }
+            println("씨ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ바")
+            skinCache[name] = profile
+            @Suppress("MagicNumber")
+            val mutableDelayData = mutableDelay(60000L)
+            skinCacheRemover[name] = mutableDelayData
+            plugin.launch {
+                mutableDelayData.block()
+                skinCache -= name
+                skinCacheRemover -= name
+            }
+        }.also {
+            profileImporter[name] = it
+            it.join()
+        }
+        println("씨바")
+        return profile
     }
 
 //    private tailrec fun tailOf(parent: Player): FakeEntity<Player>? {
--- src/main/kotlin/io/github/highright1234/humantower/kommand/HumanTowerKommand.kt
+++ src/main/kotlin/io/github/highright1234/humantower/kommand/HumanTowerKommand.kt
@@ -2,29 +2,43 @@ package io.github.highright1234.humantower.kommand
 
 import io.github.highright1234.humantower.HumanTower
 import io.github.highright1234.humantower.TowerMaker
+import io.github.highright1234.humantower.TowerMaker.skinOf
 import io.github.highright1234.shotokonoko.monun.suspendingExecutes
 import io.github.monun.kommand.PluginKommand
 import io.github.monun.kommand.getValue
+import org.bukkit.entity.Player
 
 object HumanTowerKommand : KommandClass {
     override fun register(pluginKommand: PluginKommand) {
         pluginKommand.register("tower") {
             requires { isPlayer }
-            then("size" to int()) {
-                suspendingExecutes {
-                    val size: Int by it
-                    repeat(size) {
-                        TowerMaker.create(player)
-                    }
-                }
-                then("twerking") {
-                    requires {
-                        HumanTower.plugin.server.pluginManager.isPluginEnabled("SeniorCenterTwerkMachine")
-                    }
-                    suspendingExecutes {
-                        val size: Int by it
-                        repeat(size) {
-                            TowerMaker.create(player, true)
+            then("players" to players()) {
+                then("skinOwner" to string()) {
+                    then("size" to int()) {
+                        suspendingExecutes { context ->
+                            val skinOwner: String by context
+                            val skin = skinOf(skinOwner)
+                            val size: Int by context
+                            val players: Collection<Player> by context
+                            repeat(size) {
+                                players.forEach { TowerMaker.create(it, skin) }
+                            }
+                            player.sendMessage("End")
+                        }
+                        then("twerking") {
+                            requires {
+                                HumanTower.plugin.server.pluginManager.isPluginEnabled("SeniorCenterTwerkMachine")
+                            }
+                            suspendingExecutes {context ->
+                                val skinOwner: String by context
+                                val skin = skinOf(skinOwner)
+                                val size: Int by context
+                                val players: Collection<Player> by context
+                                repeat(size) {
+                                    players.forEach { TowerMaker.create(it, skin, true) }
+                                }
+                                player.sendMessage("End")
+                            }
                         }
                     }
                 }
